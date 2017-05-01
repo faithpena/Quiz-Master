af77c8b01936c4fa59c6c09f302c3c90a2901cf9
faithpena <faithpena@gmail.com>
2017-05-01 20:20:35 +0800

    Edited GUI

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,7 +11,7 @@ class QuestionsController < ApplicationController
   def create
   	@question = Question.new(question_params)
   	if @question.save
-  		redirect_to @question
+  		redirect_to current_user
   	else
   		render 'new'
   	end
